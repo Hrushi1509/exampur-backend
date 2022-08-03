a3103ea3aab82f47e85d7753b00f34148295f5e4
Hrushi1509 <hrushikeshwalujkar@gmail.com>
2022-08-04 00:15:13 +0530

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+
+const cors = require("cors")
+// require("dotenv").config({path:".env"})
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req,res)=>{
+    try{
+        console.log("Exampur api is running")
+        res.send('<h1>Exampur api is running</h1>')
+    }
+    catch{
+
+    }
+})
+
+const usersRoute = require('./routes/users')
+app.use('/users', usersRoute)
+
+app.listen(5000)
+// app.listen(process.env.APP_PORT)
